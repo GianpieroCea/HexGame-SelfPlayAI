6634c81772c3db34ee0da561b49eb3aac727457b
dinies <edoardo.ghini@live.it>
2023-04-13 10:18:58 +0000

    add implementation and test of recursive tree expansion

---
+++ hexgame_ai/tree_creator.py
@@ -0,0 +1,28 @@
+from hexgame.color import Color
+from hexgame.board import Board
+import copy
+
+
+class NodeTree:
+    def expand_tree_rec(board: Board,
+                        action: tuple[int, int], depth: int):
+        # print("-------------------recursion-------------------\n")
+        # print(f"board:\n {board}"
+        #       f"action: {action}\n"
+        #       f"depth: {depth}\n")
+        if action:
+            current_color = Color.Red if depth % 2 == 1 else Color.Blue
+            board.place_stone(
+                action[0], action[1], current_color
+            )
+            if board._has_color_won(current_color):
+                # print(f"player {current_color} has won\n")
+                return {"data": action, "children": []}
+        possible_actions = board.empty_positions
+        # print(f"possible actions:{possible_actions}\n")
+        children = []
+        for a in possible_actions:
+            next_board = copy.deepcopy(board)
+            children.append(NodeTree.expand_tree_rec(
+                next_board, a, depth+1))
+        return {"data": action, "children": children}
